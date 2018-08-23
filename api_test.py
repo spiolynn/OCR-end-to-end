42451a5754434629504753ab5d099ce25ce4d104
spiolynn <35069534+spiolynn@users.noreply.github.com>
2018-08-23 14:44:21 +0800

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # user_info = {'name': ['letian', 'letian2'], 'password': '123'}
 
@@ -13,7 +14,8 @@ def test1():
     # r = requests.post("http://127.0.0.1:5000/ctpn", data=orc_info)
     orc_info = {'TraceId': '20180808010101-0000001','FileList': './test_images/demo1.jpg', 'Image': ''}
     r = requests.post("http://127.0.0.1:5000/ctpn", json=orc_info)
-    print(r.text)
+    js = json.loads(r.text)
+    print(js)
 
 if __name__ == '__main__':
     test1()
